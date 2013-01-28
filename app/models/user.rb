0cc5adb5dbc6f54ed88d16bfee1f30bdb688fdc8
PanfilovDenis <panf.denis@gmail.com>
2013-01-28 20:15:01 +0000

    add rails admin and cancan

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :role
   # attr_accessible :title, :body
+
+  def role?(role)
+    self.role == role.to_s
+  end
 end
