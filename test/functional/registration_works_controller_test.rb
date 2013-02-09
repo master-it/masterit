50cd622d587600c65077d7ebdf61e05d50ce46ef
login73ul <login73ul@gmail.com>
2013-02-09 21:40:18 +0400

    fixes errors

--- test/functional/registration_works_controller_test.rb
+++ test/functional/registration_works_controller_test.rb
@@ -18,7 +18,7 @@ class RegistrationWorksControllerTest < ActionController::TestCase
 
   test "should create registration_work" do
     assert_difference('RegistrationWork.count') do
-      post :create, registration_work: { area: @registration_work.area, city: @registration_work.city, class: @registration_work.class, description: @registration_work.description, email: @registration_work.email, name: @registration_work.name, nominate: @registration_work.nominate, phone: @registration_work.phone, picture: @registration_work.picture, school: @registration_work.school, url_work: @registration_work.url_work }
+      post :create, registration_work: { area: @registration_work.area, city: @registration_work.city, name_class: @registration_work.name_class, description: @registration_work.description, email: @registration_work.email, name: @registration_work.name, nominate: @registration_work.nominate, phone: @registration_work.phone, picture: @registration_work.picture, school: @registration_work.school, url_work: @registration_work.url_work }
     end
 
     assert_redirected_to registration_work_path(assigns(:registration_work))
@@ -35,7 +35,7 @@ class RegistrationWorksControllerTest < ActionController::TestCase
   end
 
   test "should update registration_work" do
-    put :update, id: @registration_work, registration_work: { area: @registration_work.area, city: @registration_work.city, class: @registration_work.class, description: @registration_work.description, email: @registration_work.email, name: @registration_work.name, nominate: @registration_work.nominate, phone: @registration_work.phone, picture: @registration_work.picture, school: @registration_work.school, url_work: @registration_work.url_work }
+    put :update, id: @registration_work, registration_work: { area: @registration_work.area, city: @registration_work.city, name_class: @registration_work.name_class, description: @registration_work.description, email: @registration_work.email, name: @registration_work.name, nominate: @registration_work.nominate, phone: @registration_work.phone, picture: @registration_work.picture, school: @registration_work.school, url_work: @registration_work.url_work }
     assert_redirected_to registration_work_path(assigns(:registration_work))
   end
 
