8c9fe5ab812c44998e0f9c2b623009182c03b62e
login73ul <login73ul@gmail.com>
2013-02-11 19:07:02 +0400

    add seeds with nominates

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,21 @@ Competition.create(year: 2013)
 Competition.create(year: 2012)
 tours = ["Школьный", "Муниципальный", "Областной"]
 tours.each{|tour| Tour.create(name: tour)}
+Nominate.create(name: "Тематический сайт")
+Nominate.create(name: "2-мерная статичная графика")
+Nominate.create(name: "3-мерная статичная графика")
+Nominate.create(name: "Анимационный ролик - 2-х мерная графика")
+Nominate.create(name: "Анимационный ролик - 3-х мерная графика")
+Nominate.create(name: "Электронные учебные пособия")
+Nominate.create(name: "Презентация в программе PowerPoint")
+Nominate.create(name: "Презентация с использованием программ (Flash, Movie Maker и т.п)")
+Nominate.create(name: "Программное творчество")
+Nominate.create(name: "Компьютерные игры")
+Nominate.create(name: "Видеомонтаж, видеосъемка")
+Nominate.create(name: "Музыка")
+Nominate.create(name: "Защита информации - номинация от лаборатории Касперского")
+Nominate.create(name: "Свободное программное обеспечение (работы, выполненные на СПО)")
+Nominate.create(name: "IT-интеграция – номинация, объединяющая не менее трех побед в разных
+номинациях (для выдающихся участников)")
+Nominate.create(name: "Реклама к юбилею Конкурса (видеоролики, графика и т.д.)")
+
