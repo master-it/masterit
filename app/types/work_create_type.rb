44a0ccaf8f48666ac3027578996e70cb437f13a5
Plugin73 <laplugin73@gmail.com>
2013-04-01 22:04:08 +0400

    translation work registration

---
+++ app/types/work_create_type.rb
@@ -0,0 +1,10 @@
+class WorkCreateType < WorksController
+  include BaseType
+
+  attr_accessible :sources_attributes, :videos_attributes, :images_attributes
+
+  accepts_nested_attributes_for :images, :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :videos, :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :sources, :reject_if => :all_blank, :allow_destroy => true
+
+end
