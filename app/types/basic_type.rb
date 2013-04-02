218da320ac20d4a94d09e5d3e1607cd08dc7457d
Plugin73 <laplugin73@gmail.com>
2013-04-02 19:18:59 +0400

    in type model instead controller

--- app/types/basic_type.rb
+++ app/types/basic_type.rb
@@ -1,9 +1,13 @@
-module BaseType
+module BasicType
   extend ActiveSupport::Concern
 
   module ClassMethods
     def model_name
       superclass.model_name
+    end
+
+    def name
+      superclass.name
     end
   end
 end
