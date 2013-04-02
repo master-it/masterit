9216071cfcd266a4b745c476f145a4dd295cecaf
Plugin73 <laplugin73@gmail.com>
2013-04-02 21:53:40 +0400

    done registrate work

--- app/types/work_create_type.rb
+++ app/types/work_create_type.rb
@@ -2,9 +2,4 @@ class WorkCreateType < Work
   include BasicType
 
   attr_accessible :sources_attributes, :videos_attributes, :images_attributes
-
-  accepts_nested_attributes_for :images, :reject_if => :all_blank, :allow_destroy => true
-  accepts_nested_attributes_for :videos, :reject_if => :all_blank, :allow_destroy => true
-  accepts_nested_attributes_for :sources, :reject_if => :all_blank, :allow_destroy => true
-
 end
