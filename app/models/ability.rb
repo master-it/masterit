590c1cbef02c6967b7cee5481630f14b7d12cee3
YuryKorotin <dreadnoughtfull@yandex.ru>
2013-04-18 12:09:04 +0400

    Adding migration and roles specification

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,10 +7,20 @@ class Ability
     if user.role? :admin
       can :access, :rails_admin
       can :dashboard
-      can :manage, :all   
-    else
+      can :manage, :all
+    elsif user.role? :manager#Кураторор
+      can :read, :all
+      can :manage, WorkNomination
+      can :manage, Work do |work|
+      end
+      can :manage, Competition
+      can :manage, Tour 
+    elsif user.role? :expert
+      can :manage, Work
+      can :manage, Competition
+      can :manage, Basket
+    elsif user.role? :participant
       can :read, :all
     end
-
   end
 end
--- db/migrate/20130418065336_create_baskets.rb
+++ db/migrate/20130418065336_create_baskets.rb
@@ -1,7 +1,9 @@
 class CreateBaskets < ActiveRecord::Migration
   def change
     create_table :baskets do |t|
-
+      t.string :state
+      t.references :users
+      t.referances :works
       t.timestamps
     end
   end
