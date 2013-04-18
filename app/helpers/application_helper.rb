8b230898a28bdb86910fc6eb164aa019dcf40ffb
login73ul <login73ul@gmail.com>
2013-04-18 22:39:34 +0400

    add menu_items and pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,36 @@
 module ApplicationHelper
+  def item(tag, name, path, link_options = {})
+    options = {}
+    options[:class] = :active if current_page?(path)
+    link = link_to(name, path, link_options)
+    content_tag(:li, link, options)
+  end
+
+  def model_item(tag, model, path, options = {})
+    item(tag, model.model_name.human, path, options)
+  end
+  
+  def union_item(tag, name, path, options ={})
+    new_name = "#{content_tag(:i, '', class: 'icon-chevron-right')} #{name}".html_safe
+    item(tag, new_name, path, options)
+  end
+  
+  def show_tree(arrange_hash, &block)
+    return if arrange_hash.empty?
+    content_tag "ul type='none'" do
+      items = arrange_hash.map do |item, children|
+        content_tag "li type='none'" do
+          html = capture item, &block
+          subtree = show_tree(children, &block)
+          [html, subtree].compact.map!(&:to_s).join.html_safe
+        end
+      end
+      items.join.html_safe
+    end
+  end
+
+  def nav_menu_items_arrange
+    MenuItem.published_arrange(2)
+  end
+  
 end
